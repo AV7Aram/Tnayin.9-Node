ae91b56f40f8334f170642433390644e6be0f732
AV_Aram <aram.vardanyan.07@bk.ru>
2025-08-07 18:49:34 +0400

    Adding PATCH and DELETE queries

--- fetch.js
+++ fetch.js
@@ -2,8 +2,18 @@ fetch('http://localhost:3000/api/users', {
     method: "POST",
     headers: { "content-type": 'application/json' },
     body: JSON.stringify({
-        id : Date.now(),
+        id: Date.now(),
         name: "Arman",
         email: "Arman@gmail.com"
     })
 }).then((res) => res.json()).then((res) => console.log(res))
+
+fetch('http://localhost:3000/api/users/1', {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email: "aram_vardanyan@gmail.com" })
+}).then(res => res.json()).then(data => console.log("Updated user:", data))
+
+fetch('http://localhost:3000/api/users/1', {
+    method: "DELETE"
+}).then(res => res.json()).then(data => console.log("Result:", data))
--- script.js
+++ script.js
@@ -12,8 +12,7 @@ http.createServer(async (req, res) => {
     } else if (req.url.match(/^\/api\/users\/\d+$/) && req.method === 'GET') {
         const id = req.url.split('/').pop();
         const users = JSON.parse(await readFile('db', 'users.json'));
-        const user = users.find(u => String(u.id) === id);
-
+        const user = users.find(u => u.id === id);
         return user
             ? sendResponse(res, 200, user)
             : sendResponse(res, 404, { message: 'User not found' });
@@ -38,12 +37,50 @@ http.createServer(async (req, res) => {
             const newUser = JSON.parse(body);
             const filePath = path.join(__dirname, 'db', 'users.json');
             const users = JSON.parse(await fs.readFile(filePath, 'utf-8'));
-            newUser.id = String(newUser.id || Date.now());
+            newUser.id = newUser.id || Date.now()
             users.push(newUser);
             await fs.writeFile(filePath, JSON.stringify(users, null, 2));
             sendResponse(res, 201, newUser);
         });
         return
+    } else if (req.url.match(/^\/api\/users\/\d+$/) && req.method === 'PATCH') {
+        const id = req.url.split('/').pop();
+        const filePath = path.join(__dirname, 'db', 'users.json');
+        const users = JSON.parse(await fs.readFile(filePath, 'utf-8'));
+        const userIndex = users.findIndex(u => u.id === id);
+        if (userIndex === -1) {
+            return sendResponse(res, 404, { message: 'User not found' });
+        }
+        let body = ''
+        req.on('data', chunk => body += chunk.toString());
+        req.on('end', async () => {
+            const updates = JSON.parse(body);
+            const originalId = users[userIndex].id;
+            users[userIndex] = {
+                ...users[userIndex],
+                ...updates,
+                id: originalId
+            };
+
+            await fs.writeFile(filePath, JSON.stringify(users, null, 2));
+            sendResponse(res, 200, users[userIndex]);
+        });
+        return;
+    } else if (req.url.match(/^\/api\/users\/\d+$/) && req.method === 'DELETE') {
+        const id = req.url.split('/').pop();
+        const filePath = path.join(__dirname, 'db', 'users.json');
+        const users = JSON.parse(await fs.readFile(filePath, 'utf-8'));
+        const userIndex = users.findIndex(u => u.id === id)
+
+        if (userIndex === -1) {
+            return sendResponse(res, 404, { message: 'User not found' });
+        }
+        const [deletedUser] = users.splice(userIndex, 1);
+        await fs.writeFile(filePath, JSON.stringify(users, null, 2));
+        sendResponse(res, 200, {
+            message: `User with id ${id} deleted`,
+            deletedUser
+        });
     }
 
     const fileName = req.url === '/' && req.method === 'GET'
