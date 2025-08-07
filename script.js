dbc073f1995141291a8678956665e73521065fbc
AV_Aram <aram.vardanyan.07@bk.ru>
2025-08-07 18:14:01 +0400

    Optimization of POST request and condition with get parameter

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const fs = require('fs').promises
+const fs = require('fs').promises;
 const path = require('path');
 const { readFile } = require('./helpers/readFile');
 const { sendResponse } = require('./helpers/sendResponse');
@@ -11,33 +11,39 @@ http.createServer(async (req, res) => {
         return sendResponse(res, 200, users);
     } else if (req.url.match(/^\/api\/users\/\d+$/) && req.method === 'GET') {
         const id = req.url.split('/').pop();
-        const data = await readFile('db', 'users.json');
+        const users = JSON.parse(await readFile('db', 'users.json'));
+        const user = users.find(u => String(u.id) === id);
 
-        if (data) {
-            const users = JSON.parse(data);
-            const user = users.find(u => u.id === id);
-
-            if (user) {
-                return sendResponse(res, 200, user);
-            } else {
-                return sendResponse(res, 404, { message: 'User not found' });
-            }
-        }
+        return user
+            ? sendResponse(res, 200, user)
+            : sendResponse(res, 404, { message: 'User not found' });
     } else if (req.url.includes('?') && req.method === 'GET') {
-        const searchPosition = req.url.indexOf('?')
-        const value = req.url.slice(searchPosition + 1).split('=').at(-1)
-        const users = JSON.parse(await readFile('db', 'users.json'))
-        const newUsers = users.filter((user) => user.name.toLowerCase().indexOf(value.toLowerCase()) > -1)
-        return sendResponse(res, 200, JSON.stringify(newUsers))
+        const urlParams = new URLSearchParams(req.url.split('?')[1]);
+
+        const nameValue = urlParams.get('name');
+        if (!nameValue) {
+            return sendResponse(res, 400, { message: 'Missing name parameter' });
+        }
+
+        const users = JSON.parse(await readFile('db', 'users.json'));
+        const filteredUsers = users.filter(user =>
+            user.name.toLowerCase().includes(nameValue.toLowerCase())
+        );
+
+        return sendResponse(res, 200, filteredUsers);
     } else if (req.url === '/api/users' && req.method === 'POST') {
-        req.on('data', async (chunk) => {
-            let body = JSON.parse(chunk.toString())
-            const users = JSON.parse(await readFile('db', 'users.json'))
-            users.push(body)
-            await fs.unlink(path.join(__dirname, 'db', 'users.json'))
-            await fs.appendFile(path.join(__dirname, 'db', 'users.json'), JSON.stringify(users))
-            res.end(JSON.stringify(users))
-        })
+        let body = ''
+        req.on('data', chunk => body += chunk.toString());
+        req.on('end', async () => {
+            const newUser = JSON.parse(body);
+            const filePath = path.join(__dirname, 'db', 'users.json');
+            const users = JSON.parse(await fs.readFile(filePath, 'utf-8'));
+            newUser.id = String(newUser.id || Date.now());
+            users.push(newUser);
+            await fs.writeFile(filePath, JSON.stringify(users, null, 2));
+            sendResponse(res, 201, newUser);
+        });
+        return
     }
 
     const fileName = req.url === '/' && req.method === 'GET'
@@ -47,8 +53,7 @@ http.createServer(async (req, res) => {
     const statusCode = fileName === 'error.html' ? 404 : 200;
     const html = await readFile('pages', fileName);
 
-    if (html) {
-        return sendResponse(res, statusCode, html, 'text/html');
-    }
-})
-    .listen(3000, () => console.log('Server is Running'));
+    return html
+        ? sendResponse(res, statusCode, html, 'text/html')
+        : sendResponse(res, 404, 'Not Found', 'text/html');
+}).listen(3000, () => console.log('Server is Running'));
